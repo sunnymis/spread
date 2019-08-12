286f4de10dbf694aed76a5399d17438cdfaa6cca
Sunny Mistry <sunnyvmistry@gmail.com>
2019-08-11 22:56:49 -0400

    Remove Starter Kit Code

--- src/constants/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  // apiKey: 'AIzaSyABnWWRCfPxeoUASV0a84879Fg-swiPpFk',
-  // authDomain: 'spread-299e9.firebaseapp.com',
-  // databaseURL: 'https://spread-299e9.firebaseio.com/',
-  // projectId: 'spread-299e9',
-  // storageBucket: 'gs://spread-299e9.appspot.com/',
-  // messagingSenderId: '993723225459',
-};
