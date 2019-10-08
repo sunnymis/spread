fb294bcea9274664449a7f051e1ca778a83e1fef
Sunny Mistry <sunnyvmistry@gmail.com>
2019-10-07 23:14:18 -0400

    Style Single Page and Create Carousel

--- src/containers/Restaurant/Single.js
+++ src/containers/Restaurant/Single.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import Rating from '@material-ui/lab/Rating';
+import { styled } from '@material-ui/styles';
 import { useRestaurants } from '../../hooks';
 import { firebase } from '../../firebase';
+import Carousel from '../../components/Carousel';
 import FormModal from './FormModal';
 
 export default function Restaurant(props) {
   const { restaurants, setRestaurants } = useRestaurants();
   const [open, setOpen] = useState(false);
 
-  const { name, location, description, tags, docId } = props.location.state;
+  const {
+    name,
+    location,
+    description,
+    tags,
+    docId,
+    photos,
+  } = props.location.state;
 
   const deleteRestaurant = () => {
     firebase
@@ -28,15 +38,35 @@ export default function Restaurant(props) {
     setOpen(false);
   }
 
+  const Div = styled('div')({
+    paddingLeft: '16px',
+  });
+
+  const Tag = styled('span')({
+    backgroundColor: '#00AEFF',
+    border: '1px solid #0099ff',
+    color: '#fff',
+    borderRadius: '10%',
+    padding: '8px',
+  });
+
   return (
-    <div>
+    <Div>
       <h1>{name}</h1>
-      <h2>{location}</h2>
-      <h2>{description}</h2>
-      {/* <img src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9'/> */}
+      <Rating name="simple-controlled" value={props.rating} readOnly />
+      
+      <p>{location}</p>
+      {tags && tags.map(t => <Tag>{t}</Tag>)}
+
+      <h2>Photos</h2>
+      {photos && <Carousel photos={photos} />}
+
+      <h2>Description</h2>
+      <p>{description}</p>
+
       <button onClick={() => deleteRestaurant()}>Delete</button>
       <button onClick={() => openModal()}>Edit</button>
       <FormModal open={open} onClose={handleOnClose} />
-    </div>
+    </Div>
   );
 }
