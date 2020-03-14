1df070d400fbfecee232dad4a193024049772b27
Sunny Mistry <sunnyvmistry@gmail.com>
2020-03-14 14:01:51 -0400

    Move Restaurant Code to Restaurant Files

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,81 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { compose } from "redux";
-import { connect } from "react-redux";
 
-import { AppState } from "../../store";
 import Restaurants from "../Restaurants";
-import getRestaurantsByUserId from "../../firebase/getRestaurantsByUserId";
-import addRestaurant from "../../firebase/addRestaurant";
-import deleteRestaurant from "../../firebase/deleteRestaurant";
-import updateRestaurant from "../../firebase/updateRestaurant";
-import RestaurantForm from '../Restaurants/Form';
 
-import { FormValues } from '../Restaurants/Form';
-
-interface Props {
-  fetchAll(docId: string): void;
-  restaurants: Restaurant[];
-  restaurantsLoading: boolean;
-  onAddClick(restaurant: Restaurant): void;
-  onDeleteClick(id: string): void;
-  onEditClick(restaurant: Restaurant): void;
-}
-
-
-
-const App: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoading, onAddClick, onDeleteClick, onEditClick }) => {
-  const initialValues: FormValues = {
-    name: 'my_restaurant',
-    location: 'upper east side',
-    rating: 5,
-    description: 'great. v good.'
-  }
-  const [formValues, setFormValues] = useState(initialValues);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editingDocId, setEditingDocId] = useState('');
-
-  useEffect(() => {
-    fetchAll("n23qMAUSzDR5GcPgQmlarnK0Ok43");
-  }, []);
-
-  const handleOnEditClick = (restaurant: Restaurant) => {
-    setIsEditing(true);
-    setEditingDocId(restaurant.docId || '')
-    setFormValues(restaurant)
-  }
+const App: React.FC = () => {
 
   return (
     <div className="App">
-      <RestaurantForm
-        isEditing={isEditing}
-        editingDocId={editingDocId}
-        formValues={formValues}
-        onAddClick={onAddClick}
-        onEditClick={onEditClick}
-        onSetIsEditing={() => setIsEditing(false)}
-      />
-      {restaurantsLoading ? (
-        <div>Loading Restaurants...</div>
-      ) : (
-          <Restaurants restaurants={restaurants} onEditClick={handleOnEditClick} onDeleteClick={onDeleteClick} />
-        )}
+      <Restaurants />
     </div>
   );
 };
 
-export const mapStateToProps = (state: AppState) => ({
-  restaurantsLoading: state.restaurants.isFetching,
-  restaurants: state.restaurants.items
-});
-
-export const mapDispatchToProps = (dispatch: any) => {
-  return {
-    fetchAll: (id: string) => dispatch(getRestaurantsByUserId(id)),
-    onAddClick: (restaurant: Restaurant) => dispatch(addRestaurant(restaurant)),
-    onDeleteClick: (id: string) => dispatch(deleteRestaurant(id)),
-    onEditClick: (restaurant: Restaurant) => dispatch(updateRestaurant(restaurant)),
-  };
-};
-
-export default compose(connect(mapStateToProps, mapDispatchToProps))(App);
+export default App;
--- src/containers/Restaurants/index.tsx
+++ src/containers/Restaurants/index.tsx
@@ -1,34 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+
+import { AppState } from "../../store";
+import getRestaurantsByUserId from "../../firebase/getRestaurantsByUserId";
+import addRestaurant from "../../firebase/addRestaurant";
+import deleteRestaurant from "../../firebase/deleteRestaurant";
+import updateRestaurant from "../../firebase/updateRestaurant";
+import Form from './Form';
+import List from './List';
+
+import { FormValues } from '../Restaurants/Form';
 
 interface Props {
+  fetchAll(docId: string): void;
   restaurants: Restaurant[];
+  restaurantsLoading: boolean;
+  onAddClick(restaurant: Restaurant): void;
   onDeleteClick(id: string): void;
   onEditClick(restaurant: Restaurant): void;
 }
 
-export default function Restaurants({ restaurants, onEditClick, onDeleteClick }: Props) {
+
+const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoading, onAddClick, onDeleteClick, onEditClick }) => {
+  const initialValues: FormValues = {
+    name: 'my_restaurant',
+    location: 'upper east side',
+    rating: 5,
+    description: 'great. v good.'
+  }
+  const [formValues, setFormValues] = useState(initialValues);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingDocId, setEditingDocId] = useState('');
+
+  useEffect(() => {
+    fetchAll("n23qMAUSzDR5GcPgQmlarnK0Ok43");
+  }, []);
+
+  const handleOnEditClick = (restaurant: Restaurant) => {
+    setIsEditing(true);
+    setEditingDocId(restaurant.docId || '')
+    setFormValues(restaurant)
+  }
+
   return (
-    <div>
-      <h1>{`Count: ${restaurants.length}`}</h1>
-      {restaurants.map(r => (
-        <div key={r.docId}>
-          <p>{r.name}</p>
-          <p>{r.description}</p>
-          <p>{r.rating}</p>
-          <p>{r.location}</p>
-          <p>{r.name}</p>
-          <pre>{JSON.stringify(r.tags)}</pre>
-          <pre>{JSON.stringify(r.images)}</pre>
-          <button onClick={() => onDeleteClick(r.docId || '')}>Delete Restaurant</button>
-          <button onClick={() => onEditClick(r)}>Edit Restaurant</button>
-          <br />
-          <hr />
-          <br />
-        </div>
-      ))}
+    <div className="App">
+      <Form
+        isEditing={isEditing}
+        editingDocId={editingDocId}
+        formValues={formValues}
+        onAddClick={onAddClick}
+        onEditClick={onEditClick}
+        onSetIsEditing={() => setIsEditing(false)}
+      />
+      {restaurantsLoading ? (
+        <div>Loading Restaurants...</div>
+      ) : (
+          <List restaurants={restaurants} onEditClick={handleOnEditClick} onDeleteClick={onDeleteClick} />
+        )}
     </div>
   );
-}
+};
 
-// TODO in future if data is hard ot update when editing a restaurant
-// check how to structure nested entities in this article https://redux.js.org/advanced/async-actions/
+export const mapStateToProps = (state: AppState) => ({
+  restaurantsLoading: state.restaurants.isFetching,
+  restaurants: state.restaurants.items
+});
+
+export const mapDispatchToProps = (dispatch: any) => {
+  return {
+    fetchAll: (id: string) => dispatch(getRestaurantsByUserId(id)),
+    onAddClick: (restaurant: Restaurant) => dispatch(addRestaurant(restaurant)),
+    onDeleteClick: (id: string) => dispatch(deleteRestaurant(id)),
+    onEditClick: (restaurant: Restaurant) => dispatch(updateRestaurant(restaurant)),
+  };
+};
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(Restaurants);
