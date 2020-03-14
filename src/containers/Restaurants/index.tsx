61389b2116166015a2d6c5988b2769db27200cb3
Sunny Mistry <sunnyvmistry@gmail.com>
2020-03-14 15:30:55 -0400

    Rename dispatch actions

--- src/containers/Restaurants/index.tsx
+++ src/containers/Restaurants/index.tsx
@@ -13,20 +13,29 @@ import List from './List';
 import { FormValues } from './Form';
 
 interface Props {
-  fetchAll(docId: string): void;
   restaurants: Restaurant[];
-  restaurantsLoading: boolean;
-  onAddClick(restaurant: Restaurant): void;
-  onDeleteClick(id: string): void;
-  onEditClick(restaurant: Restaurant): void;
+  isLoading: boolean;
+  getRestaurantsByUserId(docId: string): void;
+  addRestaurant(restaurant: Restaurant): void;
+  deleteRestaurant(id: string): void;
+  updateRestaurant(restaurant: Restaurant): void;
 }
 
-const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoading, onAddClick, onDeleteClick, onEditClick }) => {
+const Restaurants: React.FC<Props> = (props) => {
+  const {
+    restaurants,
+    isLoading,
+    getRestaurantsByUserId,
+    addRestaurant,
+    deleteRestaurant,
+    updateRestaurant,
+  } = props;
+
   const initialValues: FormValues = {
-    name: 'my_restaurant',
-    location: 'upper east side',
-    rating: 5,
-    description: 'great. v good.'
+    name: '',
+    location: '',
+    rating: 0,
+    description: ''
   }
   const [formValues, setFormValues] = useState(initialValues);
   const [isEditing, setIsEditing] = useState(false);
@@ -34,7 +43,7 @@ const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoadin
   const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
-    fetchAll("n23qMAUSzDR5GcPgQmlarnK0Ok43");
+    getRestaurantsByUserId("n23qMAUSzDR5GcPgQmlarnK0Ok43");
   }, []); // eslint-disable react-hooks/exhaustive-deps
 
   const handleOnEditClick = (restaurant: Restaurant) => {
@@ -44,11 +53,11 @@ const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoadin
   }
 
   const handleOnAddClick = (restaurant: Restaurant) => {
-    onAddClick(restaurant);
+    addRestaurant(restaurant);
     setShowForm(false);
   }
 
-  if (restaurantsLoading) {
+  if (isLoading) {
     return <h1>Loading Restaurants...</h1>
   }
 
@@ -60,7 +69,7 @@ const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoadin
           editingDocId={editingDocId}
           formValues={formValues}
           onAddClick={handleOnAddClick}
-          onEditClick={onEditClick}
+          onEditClick={updateRestaurant}
           onSetIsEditing={() => setIsEditing(false)}
         />
         <button onClick={() => setShowForm(false)}>CANCEL</button>
@@ -70,23 +79,23 @@ const Restaurants: React.FC<Props> = ({ fetchAll, restaurants, restaurantsLoadin
 
   return (
     <div className="App">
-      <List restaurants={restaurants} onEditClick={handleOnEditClick} onDeleteClick={onDeleteClick} />
+      <List restaurants={restaurants} onEditClick={handleOnEditClick} onDeleteClick={deleteRestaurant} />
       <button onClick={() => setShowForm(true)}>ADD NEW</button>
     </div>
   );
 };
 
 export const mapStateToProps = (state: AppState) => ({
-  restaurantsLoading: state.restaurants.isFetching,
+  isLoading: state.restaurants.isFetching,
   restaurants: state.restaurants.items
 });
 
 export const mapDispatchToProps = (dispatch: any) => {
   return {
-    fetchAll: (id: string) => dispatch(getRestaurantsByUserId(id)),
-    onAddClick: (restaurant: Restaurant) => dispatch(addRestaurant(restaurant)),
-    onDeleteClick: (id: string) => dispatch(deleteRestaurant(id)),
-    onEditClick: (restaurant: Restaurant) => dispatch(updateRestaurant(restaurant)),
+    getRestaurantsByUserId: (id: string) => dispatch(getRestaurantsByUserId(id)),
+    addRestaurant: (restaurant: Restaurant) => dispatch(addRestaurant(restaurant)),
+    deleteRestaurant: (id: string) => dispatch(deleteRestaurant(id)),
+    updateRestaurant: (restaurant: Restaurant) => dispatch(updateRestaurant(restaurant)),
   };
 };
 
