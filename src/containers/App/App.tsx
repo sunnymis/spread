29677bbfb2a2da940e60ce6e02757745ddb1d7b9
Sunny Mistry <sunnyvmistry@gmail.com>
2020-04-17 18:07:36 -0400

    Create Basic Home Route

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 
 import Restaurants from "../Restaurants";
+import Home from "../Home";
 import RestaurantDetails from "../Restaurants/Details";
 
 const App: React.FC = () => {
@@ -13,6 +14,9 @@ const App: React.FC = () => {
     <div>
       <Router>
         <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route exact path="/restaurants">
             <Restaurants />
           </Route>
---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Link to="/restaurants" >Restaurants</Link>
+    </>
+  );
+};
+
+export default Home;
