5d9170e56d831b6f0228eddefff56a9dfce56243
Sunny Mistry <sunnyvmistry@gmail.com>
2021-03-07 21:43:29 -0500

    Render actual List, show add form on + click

--- src/containers/Restaurants/index.tsx
+++ src/containers/Restaurants/index.tsx
@@ -1,76 +1,83 @@
 import React from "react";
 import getRestaurantsByUserId from "../../firebase/getRestaurantsByUserId";
 // import addRestaurant from "../../firebase/addRestaurant";
-// import Form from "./Form";
-// import List from "./List";
+import Form from "./Form";
+import List from "./List";
 import styles from "./restaurants.module.scss";
-// import { FormValues } from "./Form";
+import { FormValues } from "./Form";
 
-class Restaurants extends React.Component<{}, { restaurants: any[] }> {
+interface State {
+  restaurants: Restaurant[];
+  showAddNewForm: boolean;
+}
+class Restaurants extends React.Component<{}, State> {
   constructor(props: any) {
     super(props);
 
     this.state = {
       restaurants: [],
+      showAddNewForm: false,
     };
   }
   async componentDidMount() {
-    const res = await getRestaurantsByUserId("n23qMAUSzDR5GcPgQmlarnK0Ok43");
-    this.setState({ restaurants: res });
+    const restaurants = await getRestaurantsByUserId("n23qMAUSzDR5GcPgQmlarnK0Ok43");
+    this.setState({ restaurants });
   }
 
-  // const initialValues: FormValues = {
-  //   name: "",
-  //   location: "",
-  //   rating: 0,
-  //   description: "",
-  //   tags: "",
-  // };
+  initialValues: FormValues = {
+    name: "",
+    location: "",
+    rating: 0,
+    description: "",
+    tags: "",
+  };
   // const [formValues, setFormValues] = useState(initialValues);
   // const [showForm, setShowForm] = useState(false);
   // const [restaurants, setRestaurants] = useState([]);
-
-  // useEffect(() => {
-  // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   // const reset = () => {
   //   setShowForm(false);
   //   setFormValues(initialValues);
   // };
 
-  // const handleOnSubmit = (restaurant: Restaurant) => {
-  //   if (typeof restaurant.tags !== "string") {
-  //     return;
-  //   }
+  handleOnSubmit = (restaurant: Restaurant) => {
+    console.log("Submitted");
+    // if (typeof restaurant.tags !== "string") {
+    //   return;
+    // }
 
-  //   let tags = restaurant.tags.split(" ");
+    // let tags = restaurant.tags.split(" ");
 
-  //   addRestaurant({
-  //     ...restaurant,
-  //     tags,
-  //   });
+    // addRestaurant({
+    //   ...restaurant,
+    //   tags,
+    // });
 
-  //   reset();
-  // };
+    // reset();
+  };
 
   render() {
+    if (this.state.showAddNewForm) {
+      return (
+        <Form
+          formValues={this.initialValues}
+          onSubmit={this.handleOnSubmit}
+          onCancel={() => console.log("reset form")}
+        />
+      );
+    }
     return (
       <div className={styles.app}>
-        <h1>{JSON.stringify(this.state.restaurants)}</h1>
+        <div>
+          <List restaurants={this.state.restaurants} />
+          <button
+            className={styles.addButton}
+            onClick={() => this.setState({ showAddNewForm: true })}
+          >
+            <i className="material-icons md-48">add</i>
+          </button>
+        </div>
       </div>
-      //   {showForm ? (
-      //     <div>
-      //       <Form formValues={formValues} onSubmit={handleOnSubmit} onCancel={reset} />
-      //     </div>
-      //   ) : (
-      //     <div>
-      //       <List restaurants={restaurants || []} />
-      //       <button className={styles.addButton} onClick={() => setShowForm(true)}>
-      //         <i className="material-icons md-48">add</i>
-      //       </button>
-      //     </div>
-      //   )}
-      // </div>
     );
   }
 }
