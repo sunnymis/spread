dbdaa3a8c1fa68a9b247368347b66c3f269f1e66
Sunny Mistry <sunnyvmistry@gmail.com>
2021-03-10 22:39:14 -0500

    Set up tests

---
+++ src/containers/App/test/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../index";
+
+test("renders learn react link", () => {
+  // const app = render(<App />);
+  // console.log(app);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
